f7f2c9e8a4a8f5cdb126081136e89ae849a6e188
Матвей <matveygghhgg@gmail.com>
2026-02-25 18:17:42 +0300

    add two new forms:Tasks form,Dashboard form

---
+++ TaskManager/View/Forms/TasksForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TaskManager.View.Forms
+{
+    public partial class TasksForm : Form
+    {
+        public TasksForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
